6abe34724d3d94497cdc12d873cc92b01c611b69
Fedepalaush <federicopalacioush@gmail.com>
2019-10-04 23:06:57 -0300

    Modificacion en el admin

--- rotiseria/admin.py
+++ rotiseria/admin.py
@@ -1,2 +1,12 @@
 from django.contrib import admin
 
+from rotiseria.models import bloque, categoría, cliente, estadopedido, pedido, pedidoproducto, producto, rol, usuario
+admin.register(bloque)
+admin.register(categoría)
+admin.register(cliente)
+admin.register(estadopedido)
+admin.register(pedido)
+admin.register(pedidoproducto)
+admin.register(producto)
+admin.register(rol)
+admin.register(usuario)
