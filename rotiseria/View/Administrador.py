4da1dd6556eea845c5a2ced941e3e786b87dbd44
Fedepalaush <federicopalacioush@gmail.com>
2019-10-06 13:35:56 -0300

    Creacion y listado de productos

--- rotiseria/View/Administrador.py
+++ rotiseria/View/Administrador.py
@@ -1,4 +1,5 @@
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
+from django.shortcuts import render
 from rotiseria.models import Producto
 from rotiseria.forms import ProductoForm
 from django.urls import reverse_lazy
@@ -8,3 +9,13 @@ class CrearProducto (CreateView):
         form_class= ProductoForm
         template_name = 'Administrador/crearproducto.html'
         success_url = reverse_lazy('index')
+
+class ListarProducto (ListView):
+    model = Producto
+    template_name = "Administrador/listarproducto.html"
+    form_class = ProductoForm
+
+    def get(self, request, *args, **kwargs):
+        productos = Producto.objects.all()
+        context_dict = {'productos': productos}
+        return render(request, self.template_name, context=context_dict)
--- rotiseria/urls.py
+++ rotiseria/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from .View.principal import index
-from rotiseria.View.Administrador import ProductoCreate
+from rotiseria.View.Administrador import CrearProducto, ListarProducto
 urlpatterns = [
 
-path('', index, name='home'),
-path('crearproducto',ProductoCreate.as_view(), name = 'crear_producto'),
+path('', index, name='index'),
+path('crearproducto',CrearProducto.as_view(), name = 'crear_producto'),
+path('listarproducto',ListarProducto.as_view(), name = 'listar_producto'),
 ]
