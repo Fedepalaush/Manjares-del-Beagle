182b395372d1a39ae31353f8a08b3cedcc4f7894
Fedepalaush <federicopalacioush@gmail.com>
2019-10-09 19:28:25 -0300

    Creacion de los CRUDS

--- rotiseria/forms.py
+++ rotiseria/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Producto
+from .models import Producto, Categoría, Cliente, Bloque
 
 class ProductoForm(forms.ModelForm):
 
@@ -22,3 +22,49 @@ class ProductoForm(forms.ModelForm):
 			'categoria':'Categoria',
 		    'Ganancia': 'Ganancia',
 		}
+
+class CategoriaForm (forms.ModelForm):
+	class Meta:
+		model = Categoría
+
+		fields = [
+			'nombre',
+
+		]
+		labels = {
+			'nombre': 'Nombre'}
+
+
+class BloqueForm(forms.ModelForm):
+	class Meta:
+		model = Bloque
+
+		fields = [
+			'id',
+			'fecha',
+			'usuario',
+		]
+		labels = {
+			'id': 'ID',
+			'fecha': 'Fecha',
+			'usuario': 'Usuario'}
+
+
+
+class ClienteForm(forms.ModelForm):
+	class Meta:
+		model = Cliente
+
+		fields = [
+			'dni',
+			'nombre',
+			'telefono',
+			'direccion',
+		]
+		labels = {
+			'dni': 'DNI',
+			'nombre': 'Nombre',
+			'telefono': 'Telefono',
+			'direccion': 'Direccion'}
+
+
